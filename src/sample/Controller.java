3c1cf91acc32f2947d56c1c069db55049a541db1
nanonixx <nhid@elpuig.xeill.net>
2021-03-01 19:22:29 +0100

    para cuando el xml se rompa porque va a pasar

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,55 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    ToggleGroup aaa;
+    @FXML
+    RadioButton hVSh, hVSc, cVSc;
+    boolean turn = true;
+
+    public void onClick(javafx.event.ActionEvent actionEvent){
+
+        RadioButton rb = (RadioButton)aaa.getSelectedToggle();
+        if (rb!=null) {
+            if (hVSh.isSelected()) hVSh(actionEvent);
+            if (hVSc.isSelected()) hVSh(actionEvent);
+            if (cVSc.isSelected()) hVSh(actionEvent);
+        }
+
+    }
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
+    public void hVSh(javafx.event.ActionEvent actionEvent) {
+        Button b = (Button) actionEvent.getSource();
+
+        if (b.getText().equals("")){
+            if (turn) {
+                b.setText("X");
+                turn = !turn;
+            }
+            else {
+                b.setText("O");
+                turn = !turn;
+            }
+
+            //TODO método checkWin
+
+        }
+    }
 
 }
---
+++ src/sample/Game.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class Game {
+    boolean turn = true;
+
+    public void start(){
+
+
+        for (int i = 0; i < 9; i++) {
+            if (turn) {}
+            else {}
+
+            turn = !turn;
+
+        }
+    }
+}
