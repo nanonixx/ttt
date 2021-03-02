8809596408a06d3a0fd07297c1ee52808f65a357
nanonixx <nhid@elpuig.xeill.net>
2021-03-02 18:22:16 +0100

    botonsico ya no es null

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 
+import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,11 +19,11 @@ public class Controller implements Initializable {
     @FXML
     RadioButton hVSh, hVSc, cVSc;
     @FXML
-    Button b00, b01, b02, b10, b11, b12, b20, b21, b22;
+    Button b00, b01, b02, b10, b11, b12, b20, b21, b22, start;
 
-    List<Button> botonsicos = new ArrayList<>(Arrays.asList(b00, b01, b02, b10, b11, b12, b20, b21, b22));
 
     boolean turn = true;
+    int turno = 0;
 
     public void onClick(javafx.event.ActionEvent actionEvent){
 
@@ -30,9 +31,22 @@ public class Controller implements Initializable {
         if (rb!=null) {
             if (hVSh.isSelected()) hVSh(actionEvent);
             if (hVSc.isSelected()) hVSc(actionEvent);
-            if (cVSc.isSelected()) hVSh(actionEvent);
         }
 
+    }
+
+    public void startButton() {
+        reset();
+
+        RadioButton rb = (RadioButton)aaa.getSelectedToggle();
+        if (rb!=null) {
+            if (cVSc.isSelected()) cVSc();
+        }
+
+    }
+
+    private void reset() {
+        b00.setText("");
     }
 
 
@@ -64,24 +78,64 @@ public class Controller implements Initializable {
         Button b = (Button) actionEvent.getSource();
         Button botonsico;
         boolean valid = false;
+        List<Button> botonsicos = new ArrayList<>(Arrays.asList(b00, b01, b02, b10, b11, b12, b20, b21, b22));
+
 
         if (b.getText().equals("")){
                 b.setText("X");
 
 
-                while (!valid){
-                    int randomNum = (int) (Math.random() * 8);
+                while (!valid && turno <= 3){
+                    int randomNum = (int) (Math.random() * 9);
                     botonsico = botonsicos.get(randomNum);
 
                     if (botonsico.getText().equals("")){
                         botonsico.setText("O");
                         valid = true;
+                        turno++;
                     }
 
                 }
                 }
 
-                }
+        }
 
-    }
+
+        public void cVSc() {
+            while (turno < 9){
+
+                System.out.println(turno);
+            List<Button> botonsicos = new ArrayList<>(Arrays.asList(b00, b01, b02, b10, b11, b12, b20, b21, b22));
+            Button botonsico;
+            System.out.println(botonsicos.toString());
+
+            boolean valid = false;
+
+            while(!valid){
+                int randomNum = (int) (Math.random() * 9);
+                botonsico = botonsicos.get(randomNum);
+
+            if (botonsico.getText().equals("")) {
+                if (turn) {
+                    botonsico.setText("X");
+                    turn = !turn;
+                } else {
+                    botonsico.setText("O");
+                    turn = !turn;
+                }
+                valid = true;
+            }
+
+                //TODO método checkWin
+            }
+                turno++;
+
+            }
+            System.out.println("fin del while");
+
+            }
+
+
+
+        }
 
