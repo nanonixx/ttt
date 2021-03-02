9e624f1191b2b28c60c57616951c3f18ae5240ee
nanonixx <nhid@elpuig.xeill.net>
2021-03-02 19:07:58 +0100

    haciendo win

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 
@@ -20,10 +21,13 @@ public class Controller implements Initializable {
     RadioButton hVSh, hVSc, cVSc;
     @FXML
     Button b00, b01, b02, b10, b11, b12, b20, b21, b22, start;
+    @FXML
+    Label win;
 
 
     boolean turn = true;
     int turno = 0;
+    boolean finish = false;
 
     public void onClick(javafx.event.ActionEvent actionEvent){
 
@@ -46,7 +50,39 @@ public class Controller implements Initializable {
     }
 
     private void reset() {
+        finish = false;
+        turno = 0;
         b00.setText("");
+        b01.setText("");
+        b02.setText("");
+        b10.setText("");
+        b11.setText("");
+        b12.setText("");
+        b20.setText("");
+        b21.setText("");
+        b22.setText("");
+    }
+
+    private boolean checkWin(String letra) {
+        //howizontales
+        if (b00.getText().equals(letra) && b01.getText().equals(letra) && b02.getText().equals(letra) ) return true;
+        else if (b10.getText().equals(letra) && b11.getText().equals(letra) && b12.getText().equals(letra) ) return true;
+        else if (b20.getText().equals(letra) && b21.getText().equals(letra) && b22.getText().equals(letra) ) return true;
+
+            //verticales
+
+        else if (b00.getText().equals(letra) && b10.getText().equals(letra) && b20.getText().equals(letra) ) return true;
+        else if (b01.getText().equals(letra) && b11.getText().equals(letra) && b21.getText().equals(letra) ) return true;
+        else if (b02.getText().equals(letra) && b12.getText().equals(letra) && b22.getText().equals(letra) ) return true;
+
+
+        //diagonales
+
+        else if (b00.getText().equals(letra) && b11.getText().equals(letra) && b22.getText().equals(letra) ) return true;
+        else if (b20.getText().equals(letra) && b11.getText().equals(letra) && b20.getText().equals(letra) ) return true;
+
+        else return false;
+
     }
 
 
@@ -62,14 +98,16 @@ public class Controller implements Initializable {
         if (b.getText().equals("")){
             if (turn) {
                 b.setText("X");
+                if (checkWin("X")) finish = true;
                 turn = !turn;
             }
             else {
                 b.setText("O");
+                if (checkWin("O")) finish = true;
                 turn = !turn;
             }
 
-            //TODO método checkWin
+            if (finish) System.out.println("win!");
 
         }
     }
@@ -104,10 +142,8 @@ public class Controller implements Initializable {
         public void cVSc() {
             while (turno < 9){
 
-                System.out.println(turno);
             List<Button> botonsicos = new ArrayList<>(Arrays.asList(b00, b01, b02, b10, b11, b12, b20, b21, b22));
             Button botonsico;
-            System.out.println(botonsicos.toString());
 
             boolean valid = false;
 
@@ -131,7 +167,6 @@ public class Controller implements Initializable {
                 turno++;
 
             }
-            System.out.println("fin del while");
 
             }
 
